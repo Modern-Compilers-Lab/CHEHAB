7ef2f396dbbf89041a63c69483a8cc4d645a15c2
nassimb0u <in_boulechfar@esi.dz>
2022-11-22 10:47:42 +0100

    consider initial pmd and its size as parameters
    Improve bit sizes initial value (was {0, 0})

--- ufhe/benchmarks/params_simulation/main.cpp
+++ ufhe/benchmarks/params_simulation/main.cpp
@@ -1,66 +1,10 @@
-#include "seal/seal.h"
+#include "utils.hpp"
 #include <cstddef>
-#include <iostream>
 #include <random>
 #include <vector>
 
 using namespace std;
 using namespace seal;
-
-/*
-Helper function: Prints the parameters in a SEALContext.
-*/
-inline void print_parameters(const seal::SEALContext &context)
-{
-  auto &context_data = *context.key_context_data();
-
-  /*
-  Which scheme are we using?
-  */
-  std::string scheme_name;
-  switch (context_data.parms().scheme())
-  {
-  case seal::scheme_type::bfv:
-    scheme_name = "BFV";
-    break;
-  case seal::scheme_type::ckks:
-    scheme_name = "CKKS";
-    break;
-  case seal::scheme_type::bgv:
-    scheme_name = "BGV";
-    break;
-  default:
-    throw std::invalid_argument("unsupported scheme");
-  }
-  std::cout << "/" << std::endl;
-  std::cout << "| Encryption parameters :" << std::endl;
-  std::cout << "|   scheme: " << scheme_name << std::endl;
-  std::cout << "|   poly_modulus_degree: " << context_data.parms().poly_modulus_degree() << std::endl;
-
-  /*
-  Print the size of the true (product) coefficient modulus.
-  */
-  std::cout << "|   coeff_modulus size: ";
-  std::cout << context_data.total_coeff_modulus_bit_count() << " (";
-  auto coeff_modulus = context_data.parms().coeff_modulus();
-  std::size_t coeff_modulus_size = coeff_modulus.size();
-  for (std::size_t i = 0; i < coeff_modulus_size - 1; ++i)
-  {
-    std::cout << coeff_modulus[i].bit_count() << " + ";
-  }
-  std::cout << coeff_modulus.back().bit_count();
-  std::cout << ") bits" << std::endl;
-
-  /*
-  For the BFV scheme print the plain_modulus parameter.
-  */
-  if (context_data.parms().scheme() == seal::scheme_type::bfv)
-  {
-    std::cout << "|   plain_modulus: " << context_data.parms().plain_modulus().value() << std::endl;
-  }
-
-  std::cout << "\\" << std::endl;
-}
 
 void test_params(EncryptionParameters params, int xdepth)
 {
@@ -90,20 +34,18 @@ void test_params(EncryptionParameters params, int xdepth)
   first (key level special modulus) and last (lowest level) modulus.
  */
   int nb_mod_switch = params.coeff_modulus().size() - 2;
-
+  cout << "init noise bud " << decryptor.invariant_noise_budget(encrypted) << endl;
   for (int i = 0; i < xdepth; ++i)
   {
     evaluator.multiply_inplace(encrypted, encrypted);
     if (decryptor.invariant_noise_budget(encrypted) == 0)
       throw invalid_argument("insufficient noise budget");
     evaluator.relinearize_inplace(encrypted, relin_keys);
+    cout << "noise bud " << i << " " << decryptor.invariant_noise_budget(encrypted) << endl;
+    /*
     // Do mod_switch operation nb_mod_switch times evenly well distributed across xdepth consecutive multiplications
     if (nb_mod_switch && i / (xdepth / nb_mod_switch) != (i + 1) / (xdepth / nb_mod_switch))
-    {
-      cout << "noise bud " << decryptor.invariant_noise_budget(encrypted) << endl;
-      evaluator.mod_switch_to_next_inplace(encrypted);
-      cout << "noise bud " << decryptor.invariant_noise_budget(encrypted) << endl;
-    }
+      evaluator.mod_switch_to_next_inplace(encrypted);*/
   }
   // Print valid parameters
   print_parameters(context);
@@ -114,25 +56,25 @@ int main()
   scheme_type scheme = scheme_type::bfv;
   sec_level_type sec_level = sec_level_type::tc128;
   int plain_modulus_size = 20;
-  int xdepth = 5;
-
-  size_t poly_modulus_degree = 16384;
+  int xdepth = 3;
+  size_t initial_pmd = 1024;
+  int initial_pmd_bit_size = 11;
   size_t max_pmd = 32768;
-  int pmd_bit_size = 11;
+
+  size_t poly_modulus_degree = initial_pmd;
   int max_bit_count = CoeffModulus::MaxBitCount(poly_modulus_degree);
 
   auto increase_pmd = [&]() {
     if (poly_modulus_degree >= max_pmd)
       throw logic_error("parameters for this xdepth could not be selected using the security standard");
     poly_modulus_degree *= 2;
-    ++pmd_bit_size;
     max_bit_count = CoeffModulus::MaxBitCount(poly_modulus_degree);
   };
 
   vector<Modulus> coeff_modulus;
-  // Initial zero bit_sizes: data level modulus, special modulus
-  vector<int> bit_sizes{49, 49, 49, 49, 49};
-  int bit_count = 245;
+  // Initial bit_sizes: one data level prime, special modulus
+  vector<int> bit_sizes{initial_pmd_bit_size, initial_pmd_bit_size};
+  int bit_count = 2 * initial_pmd_bit_size;
   Modulus plain_modulus;
 
   do
