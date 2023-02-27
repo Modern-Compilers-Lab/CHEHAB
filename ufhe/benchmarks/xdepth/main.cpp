473af64be5ed470dbc15c1e5730c811834f73226
nassimb0u <in_boulechfar@esi.dz>
2023-02-27 19:12:32 +0100

    noise growth experiments

--- ufhe/benchmarks/xdepth/main.cpp
+++ ufhe/benchmarks/xdepth/main.cpp
@@ -1,7 +1,7 @@
 #include "seal/seal.h"
-#include "utils.hpp"
 #include <chrono>
 #include <cstddef>
+#include <fstream>
 #include <iostream>
 #include <random>
 #include <stdexcept>
@@ -10,96 +10,92 @@
 using namespace std;
 using namespace seal;
 
-EncryptionParameters select_params_bfv(
-  int xdepth, bool use_least_levels = false, sec_level_type sec_level = sec_level_type::tc128,
-  size_t initial_poly_md = 2048, int initial_plain_m_size = 17)
+vector<vector<chrono::microseconds>> mul_benchmark(SEALContext context, int xdepth, bool mod_switch = false);
+void print_parameters(const seal::SEALContext &context);
+
+int main(int argc, char **argv)
 {
-  if (initial_plain_m_size < 17)
-    throw invalid_argument("plain modulus size must be at least 17 bits");
+  int plain_mod_size = 17;
+  if (argc > 1)
+    plain_mod_size = atoi(argv[1]);
 
-  size_t max_poly_md = 32768;
-  int max_fresh_noise = 10;
-  int safety_margin = 1;
+  int xdepth = 10;
+  if (argc > 2)
+    xdepth = atoi(argv[2]);
 
-  size_t poly_modulus_degree = initial_poly_md;
-  vector<Modulus> coeff_modulus;
-  Modulus plain_modulus;
-  int plain_m_size = initial_plain_m_size;
+  int coeff_mod_data_level_size = 333;
+  if (argc > 3)
+    coeff_mod_data_level_size = atoi(argv[3]);
 
-  // Set coeff modulus primes sizes
-  int worst_case_mul_ng = util::get_significant_bit_count(poly_modulus_degree) * 2 + plain_m_size + 1;
-  int best_case_mul_ng = plain_m_size + 1;
-  int avg_mul_ng = (worst_case_mul_ng + best_case_mul_ng) / 2 + (worst_case_mul_ng + best_case_mul_ng) % 2;
-  int coeff_m_data_level_bc = plain_m_size + max_fresh_noise + xdepth * avg_mul_ng + safety_margin;
+  bool mod_switch = false;
+  int repeat = 20;
 
-  vector<int> coeff_m_bit_sizes;
-  // No special prime
-  if (xdepth <= 1 && coeff_m_data_level_bc <= 60)
-    coeff_m_bit_sizes.assign(1, coeff_m_data_level_bc);
-  else
+  string id = to_string(plain_mod_size) + "_" + to_string(xdepth) + "_" + to_string(coeff_mod_data_level_size);
+  ifstream params_file("params_simulation/saved_params/" + id + ".params", ios::binary);
+  EncryptionParameters params;
+  params.load(params_file);
+  params_file.close();
+  SEALContext context(params, true, sec_level_type::tc128);
+  print_parameters(context);
+
+  string mod_switch_path = mod_switch ? "with_mod_switch/" : "without_mod_switch/";
+  ofstream mul_file;
+  mul_file.open("time_logging/" + mod_switch_path + "mul/" + id + ".txt", ios_base::app);
+  ofstream relin_file;
+  relin_file.open("time_logging/" + mod_switch_path + "relin/" + id + ".txt", ios_base::app);
+  ofstream mod_swich_file;
+  if (mod_switch)
+    mod_swich_file.open("time_logging/with_mod_switch/mod_switch" + id + ".txt", ios_base::app);
+
+  for (int i = 0; i < repeat; ++i)
   {
-    if (use_least_levels)
+    vector<vector<chrono::microseconds>> results = mul_benchmark(context, xdepth, mod_switch);
+
+    vector<chrono::microseconds> &time_mul_levels = results[0];
+    vector<chrono::microseconds> &time_relin_levels = results[1];
+    vector<chrono::microseconds> &time_mod_switch_levels = results[2];
+
+    // Print resutls
+    cout << "mul: ";
+    for (int i = 0; i < xdepth; ++i)
     {
-      int lowest_nb_levels = coeff_m_data_level_bc / 60;
-      if (coeff_m_data_level_bc % 60)
-        ++lowest_nb_levels;
-      coeff_m_bit_sizes.assign(lowest_nb_levels, 60);
-      // Remove exceeding bits
-      for (int i = 0; i < 60 - (coeff_m_data_level_bc % 60); ++i)
-        --coeff_m_bit_sizes[i % coeff_m_bit_sizes.size()];
-      // Add special modulus for ks
-      coeff_m_bit_sizes.push_back(coeff_m_bit_sizes.back());
+      mul_file << time_mul_levels[i].count() << " ";
+      cout << time_mul_levels[i].count() << " ";
     }
-    else
+    mul_file << endl;
+    cout << endl;
+
+    cout << "relin: ";
+    for (int i = 0; i < xdepth; ++i)
     {
-      int avg_prime_size = coeff_m_data_level_bc / xdepth;
-      coeff_m_bit_sizes.assign(xdepth, avg_prime_size);
-      // Remove exceeding bits
-      for (int i = 0; i < coeff_m_data_level_bc % xdepth; ++i)
-        ++coeff_m_bit_sizes.end()[-1 - i];
-      // Add special modulus for ks
-      coeff_m_bit_sizes.push_back(coeff_m_bit_sizes.back());
+      relin_file << time_relin_levels[i].count() << " ";
+      cout << time_relin_levels[i].count() << " ";
     }
-  }
+    relin_file << endl;
+    cout << endl;
 
-  // If for the supposed n and the given security level q size cannot reach the needed value
-  if (
-    CoeffModulus::MaxBitCount(poly_modulus_degree, sec_level) <
-    coeff_m_data_level_bc + ((coeff_m_bit_sizes.size() > 1) ? coeff_m_bit_sizes.back() : 0))
-  {
-    if (poly_modulus_degree == max_poly_md)
-      throw invalid_argument("depth too large, corresponding parameters not included in fhe security standard");
-    //  Repeat with initial n=2*current_n
-    return select_params_bfv(xdepth, use_least_levels, sec_level, poly_modulus_degree * 2, plain_m_size);
+    if (mod_switch)
+    {
+      cout << "modswitch: ";
+      for (int i = 0; i < xdepth; ++i)
+      {
+        mod_swich_file << time_mod_switch_levels[i].count() << " ";
+        cout << time_mod_switch_levels[i].count() << " ";
+      }
+      mod_swich_file << endl;
+      cout << endl;
+    }
+    cout << endl;
   }
-
-  try
-  {
-    plain_modulus = PlainModulus::Batching(poly_modulus_degree, plain_m_size);
-  }
-  // suitable prime could not be found
-  catch (logic_error &e)
-  {
-    if (plain_m_size == 60)
-      throw invalid_argument("depth too large, corresponding parameters not included in fhe security standard");
-    //  Repeat with incremented initial plain modulus size
-    return select_params_bfv(xdepth, use_least_levels, sec_level, poly_modulus_degree, plain_m_size + 1);
-  }
-
-  coeff_modulus = CoeffModulus::Create(poly_modulus_degree, coeff_m_bit_sizes);
-  // Create context from the selected parameters
-  EncryptionParameters params(scheme_type::bfv);
-  params.set_poly_modulus_degree(poly_modulus_degree);
-  params.set_coeff_modulus(coeff_modulus);
-  params.set_plain_modulus(plain_modulus);
-  return params;
+  mul_file.close();
+  relin_file.close();
+  if (mod_switch)
+    mod_swich_file.close();
+  return 0;
 }
 
-void mul_benchmark(SEALContext context, int xdepth, int repeat = 10)
+vector<vector<chrono::microseconds>> mul_benchmark(SEALContext context, int xdepth, bool mod_switch)
 {
-  if (context.first_context_data()->parms().coeff_modulus().size() != xdepth)
-    throw invalid_argument("Coeff_modulus levels different than xdepth");
-
   chrono::high_resolution_clock::time_point time_start, time_end;
   vector<chrono::microseconds> time_mul_levels(xdepth);
   vector<chrono::microseconds> time_relin_levels(xdepth);
@@ -126,44 +122,21 @@ void mul_benchmark(SEALContext context, int xdepth, int repeat = 10)
   Ciphertext encrypted(context);
   encryptor.encrypt(plain, encrypted);
 
-  Ciphertext tmp(context);
-
   for (int i = 0; i < xdepth; i++)
   {
     // Multiplication time per level
-    for (int j = 0; j < repeat - 1; ++j)
-    {
-      time_start = chrono::high_resolution_clock::now();
-      evaluator.multiply(encrypted, encrypted, tmp);
-      time_end = chrono::high_resolution_clock::now();
-      time_mul_levels[i] += chrono::duration_cast<chrono::microseconds>(time_end - time_start);
-    }
     time_start = chrono::high_resolution_clock::now();
     evaluator.multiply_inplace(encrypted, encrypted);
     time_end = chrono::high_resolution_clock::now();
-    time_mul_levels[i] += chrono::duration_cast<chrono::microseconds>(time_end - time_start);
+    time_mul_levels[i] = chrono::duration_cast<chrono::microseconds>(time_end - time_start);
     // Relinearization time per level
-    for (int j = 0; j < repeat - 1; ++j)
-    {
-      time_start = chrono::high_resolution_clock::now();
-      evaluator.relinearize(encrypted, relin_keys, tmp);
-      time_end = chrono::high_resolution_clock::now();
-      time_relin_levels[i] += chrono::duration_cast<chrono::microseconds>(time_end - time_start);
-    }
     time_start = chrono::high_resolution_clock::now();
     evaluator.relinearize_inplace(encrypted, relin_keys);
     time_end = chrono::high_resolution_clock::now();
-    time_relin_levels[i] += chrono::duration_cast<chrono::microseconds>(time_end - time_start);
-    // Modulus switching time per level
-    if (i < xdepth - 1)
+    time_relin_levels[i] = chrono::duration_cast<chrono::microseconds>(time_end - time_start);
+    if (mod_switch)
     {
-      for (int j = 0; j < repeat - 1; ++j)
-      {
-        time_start = chrono::high_resolution_clock::now();
-        evaluator.mod_switch_to_next(encrypted, tmp);
-        time_end = chrono::high_resolution_clock::now();
-        time_mod_switch_levels[i] += chrono::duration_cast<chrono::microseconds>(time_end - time_start);
-      }
+      // Modulus switching time per level
       time_start = chrono::high_resolution_clock::now();
       evaluator.mod_switch_to_next_inplace(encrypted);
       time_end = chrono::high_resolution_clock::now();
@@ -173,52 +146,63 @@ void mul_benchmark(SEALContext context, int xdepth, int repeat = 10)
   if (decryptor.invariant_noise_budget(encrypted) == 0)
     throw logic_error("insufficient noise budget");
 
-  vector<int64_t> avg_mul_levels(xdepth);
-  vector<int64_t> avg_relin_levels(xdepth);
-  vector<int64_t> avg_mod_switch_levels(xdepth);
-  for (int i = 0; i < xdepth; ++i)
-  {
-    avg_mul_levels[i] = time_mul_levels[i].count() / repeat;
-    avg_relin_levels[i] = time_mul_levels[i].count() / repeat;
-    avg_mod_switch_levels[i] = time_mul_levels[i].count() / repeat;
-  }
-
-  // Print resutls
-  cout << "Average mul: ";
-  for (int i = 0; i < xdepth; ++i)
-    cout << avg_mul_levels[i] << " " << endl;
-  cout << "Average relin: ";
-  for (int i = 0; i < xdepth; ++i)
-    cout << avg_relin_levels[i] << " " << endl;
-  cout << "Average modswitch: ";
-  for (int i = 0; i < xdepth; ++i)
-    cout << avg_mod_switch_levels[i] << " " << endl;
+  return {time_mul_levels, time_relin_levels, time_mod_switch_levels};
 }
 
-int main(int argc, char **argv)
+void print_parameters(const seal::SEALContext &context)
 {
-  // FHE security standard allows up to 23
-  int xdepth = 5;
-  bool use_least_levels = false;
-  if (argc > 1)
+  auto &context_data = *context.key_context_data();
+
+  /*
+  Which scheme are we using?
+  */
+  std::string scheme_name;
+  switch (context_data.parms().scheme())
   {
-    xdepth = atoi(argv[1]);
-    if (argc > 2)
-      use_least_levels = atoi(argv[2]);
+  case seal::scheme_type::bfv:
+    scheme_name = "BFV";
+    break;
+  case seal::scheme_type::ckks:
+    scheme_name = "CKKS";
+    break;
+  case seal::scheme_type::bgv:
+    scheme_name = "BGV";
+    break;
+  default:
+    throw std::invalid_argument("unsupported scheme");
   }
-  /*EncryptionParameters parms(scheme_type::bfv);
-  size_t poly_modulus_degree = 16384;
-  parms.set_poly_modulus_degree(poly_modulus_degree);
-  parms.set_coeff_modulus(CoeffModulus::Create(poly_modulus_degree, {55, 54, 54, 54, 55, 55, 55, 55}));
-  Modulus plain_modulus = PlainModulus::Batching(poly_modulus_degree, 20);
-  parms.set_plain_modulus(plain_modulus);
-  SEALContext context(parms);
-  bfv_test(context, 10);
-  // cout << "Average mul: " << avg_mul << " microseconds" << endl;
-  // cout << "Average relin: " << avg_relin << " microseconds" << endl;*/
-  EncryptionParameters params = select_params_bfv(xdepth, use_least_levels);
-  SEALContext context(params);
-  print_parameters(context);
-  mul_benchmark(context, xdepth);
-  return 0;
+  std::cout << "/" << std::endl;
+  std::cout << "| Encryption parameters :" << std::endl;
+  std::cout << "|   scheme: " << scheme_name << std::endl;
+  size_t poly_mod = context_data.parms().poly_modulus_degree();
+  std::cout << "|   poly_mod: " << poly_mod << " (" << util::get_significant_bit_count(poly_mod) << " bits)"
+            << std::endl;
+
+  /*
+  Print the size of the true (product) coefficient modulus.
+  */
+  std::cout << "|   coeff_mod size: ";
+  auto coeff_mod = context_data.parms().coeff_modulus();
+  int total_bit_count = context_data.total_coeff_modulus_bit_count();
+  std::cout << total_bit_count << " (" << total_bit_count - coeff_mod.back().bit_count() << " + "
+            << coeff_mod.back().bit_count() << ") (";
+  std::size_t coeff_mod_size = coeff_mod.size();
+  for (std::size_t i = 0; i < coeff_mod_size - 1; ++i)
+  {
+    std::cout << coeff_mod[i].bit_count() << " + ";
+  }
+  std::cout << coeff_mod.back().bit_count();
+  std::cout << ") bits" << std::endl;
+
+  /*
+  For BFV and BGV schemes print the plain_mod parameter.
+  */
+  if (
+    context_data.parms().scheme() == seal::scheme_type::bfv || context_data.parms().scheme() == seal::scheme_type::bgv)
+  {
+    Modulus plain_mod = context_data.parms().plain_modulus();
+    std::cout << "|   plain_mod: " << plain_mod.value() << " (" << plain_mod.bit_count() << " bits)" << std::endl;
+  }
+
+  std::cout << "\\" << std::endl;
 }
--- ufhe/benchmarks/xdepth/params_simulation/serialize_params.cpp
+++ ufhe/benchmarks/xdepth/params_simulation/serialize_params.cpp
@@ -8,59 +8,31 @@ using namespace seal;
 int main(int argc, char **argv)
 {
   EncryptionParameters params(scheme_type::bfv);
-  string id;
 
-  bool from_xdepth = false;
+  int plain_mod_size = 17;
   if (argc > 1)
-    from_xdepth = atoi(argv[1]);
+    plain_mod_size = atoi(argv[1]);
 
-  if (from_xdepth)
-  {
-    int init_plain_mod_size = 14;
-    if (argc > 2)
-      init_plain_mod_size = atoi(argv[2]);
+  int xdepth = 10;
+  if (argc > 2)
+    xdepth = atoi(argv[2]);
 
-    int xdepth = 10;
-    if (argc > 3)
-      xdepth = atoi(argv[3]);
+  int coeff_mod_data_level_size = 333;
+  if (argc > 3)
+    coeff_mod_data_level_size = atoi(argv[3]);
 
-    int safety_margin = xdepth;
-    if (argc > 4)
-      safety_margin = atoi(argv[4]);
+  string id = to_string(plain_mod_size) + "_" + to_string(xdepth) + "_" + to_string(coeff_mod_data_level_size);
 
-    const SEALContext &context = get<0>(bfv_params_simulation(init_plain_mod_size, xdepth, safety_margin));
-    auto &context_data = *context.key_context_data();
-    params = context_data.parms();
-    id = to_string(params.plain_modulus().bit_count()) + "_" + to_string(xdepth) + "_" +
-         to_string(context_data.total_coeff_modulus_bit_count() - params.coeff_modulus().back().bit_count());
-  }
-  else
-  {
-    int plain_mod_size = 17;
-    if (argc > 2)
-      plain_mod_size = atoi(argv[2]);
-
-    int coeff_mod_data_level_size = 333;
-    if (argc > 3)
-      coeff_mod_data_level_size = atoi(argv[3]);
-
-    int xdepth = 10;
-    if (argc > 4)
-      xdepth = atoi(argv[4]);
-
-    id = to_string(plain_mod_size) + "_" + to_string(xdepth) + "_" + to_string(coeff_mod_data_level_size);
-
-    vector<int> coeff_mod_primes_sizes = split_coeff_mod_lowest_nb_primes(coeff_mod_data_level_size);
-    int coeff_mod_total_size = coeff_mod_data_level_size + coeff_mod_primes_sizes.back();
-    size_t poly_mod = get_poly_mod(coeff_mod_total_size, 1024);
-    vector<Modulus> coeff_mod = CoeffModulus::Create(poly_mod, coeff_mod_primes_sizes);
-    Modulus plain_mod = PlainModulus::Batching(poly_mod, plain_mod_size);
-    params.set_poly_modulus_degree(poly_mod);
-    params.set_coeff_modulus(coeff_mod);
-    params.set_plain_modulus(plain_mod);
-    SEALContext context(params);
-    print_parameters(context);
-  }
+  vector<int> coeff_mod_primes_sizes = split_coeff_mod_lowest_nb_primes(coeff_mod_data_level_size);
+  int coeff_mod_total_size = coeff_mod_data_level_size + coeff_mod_primes_sizes.back();
+  size_t poly_mod = get_poly_mod(coeff_mod_total_size, 1024);
+  vector<Modulus> coeff_mod = CoeffModulus::Create(poly_mod, coeff_mod_primes_sizes);
+  Modulus plain_mod = PlainModulus::Batching(poly_mod, plain_mod_size);
+  params.set_poly_modulus_degree(poly_mod);
+  params.set_coeff_modulus(coeff_mod);
+  params.set_plain_modulus(plain_mod);
+  SEALContext context(params);
+  print_parameters(context);
 
   string ofile_name = "saved_params/" + id + ".params";
   ofstream ofile(ofile_name, ios::binary);
