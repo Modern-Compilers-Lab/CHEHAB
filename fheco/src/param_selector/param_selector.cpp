eb0132447202e06ca3c580436a9fb46f9363e7a1
nassimb0u <in_boulechfar@esi.dz>
2023-03-15 20:59:33 +0100

    separate modswitch insertion

--- fheco/src/param_selector/param_selector.cpp
+++ fheco/src/param_selector/param_selector.cpp
@@ -17,12 +17,14 @@ unordered_map<fhecompiler::SecurityLevel, unordered_map<size_t, int>> ParameterS
 map<int, map<size_t, ParameterSelector::NoiseEstimatesValue>> ParameterSelector::bfv_noise_estimates_seal = {
   {17, {{1024, {8, 30, 22}}, {2048, {8, 31, 23}}, {4096, {8, 32, 24}}, {8192, {8, 33, 25}}}}};
 
-void ParameterSelector::select_params()
+void ParameterSelector::select_params(bool use_mod_switch)
 {
+  program_->get_dataflow_sorted_nodes(true);
+
   switch (program_->get_scheme())
   {
   case fhecompiler::Scheme::bfv:
-    select_params_bfv();
+    select_params_bfv(use_mod_switch);
     break;
 
   case fhecompiler::Scheme::none:
@@ -106,7 +108,8 @@ void ParameterSelector::select_params_bfv(bool use_mod_switch)
     params.increase_coeff_mod_bit_sizes(MOD_BIT_COUNT_MAX);
 
   if (use_mod_switch)
-    insert_mod_switch_bfv(params.coeff_mod_data_level_bit_sizes(), nodes_noise);
+    insert_mod_switch_bfv(
+      params.coeff_mod_data_level_bit_sizes(), nodes_noise, poly_modulus_degree_noise_estimates_it->second.fresh_noise);
 
   program_->set_params(params);
 }
@@ -143,14 +146,13 @@ int ParameterSelector::simulate_noise_bfv(
 
   int circuit_noise = fresh_noise;
 
-  const auto &nodes = program_->get_dataflow_sorted_nodes(true);
+  const auto &nodes = program_->get_dataflow_sorted_nodes(false);
   for (const auto &node : nodes)
   {
     if (node->get_term_type() != ir::TermType::ciphertext)
       continue;
 
-    auto it_node = nodes_noise.find(node->get_label());
-    if (it_node != nodes_noise.end())
+    if (nodes_noise.find(node->get_label()) != nodes_noise.end())
       throw logic_error("repeated node in dataflow_sorted_nodes");
 
     if (!node->is_operation_node())
@@ -162,24 +164,26 @@ int ParameterSelector::simulate_noise_bfv(
       {
         const auto &arg1 = node->get_operands()[0];
         const auto &arg2 = node->get_operands()[1];
-        auto arg1_it = nodes_noise.find(arg1->get_label());
-        auto arg2_it = nodes_noise.find(arg2->get_label());
 
-        int arg1_noise = 0;
-        int arg2_noise = 0;
         bool is_cipher = false;
+        int arg1_noise = 0;
+        auto arg1_it = nodes_noise.find(arg1->get_label());
         if (arg1->get_term_type() == ir::TermType::ciphertext)
         {
-          is_cipher = true;
           if (arg1_it == nodes_noise.end())
             throw logic_error("parent handled before child");
+
+          is_cipher = true;
           arg1_noise = arg1_it->second;
         }
+        int arg2_noise = 0;
+        auto arg2_it = nodes_noise.find(arg2->get_label());
         if (arg2->get_term_type() == ir::TermType::ciphertext)
         {
-          is_cipher = true;
           if (arg2_it == nodes_noise.end())
             throw logic_error("parent handled before child");
+
+          is_cipher = true;
           arg2_noise = arg2_it->second;
         }
 
@@ -189,10 +193,7 @@ int ParameterSelector::simulate_noise_bfv(
         auto noise_growth_it =
           operations_noise_growth.find({node->get_opcode(), arg1->get_term_type(), arg2->get_term_type()});
         if (noise_growth_it == operations_noise_growth.end())
-          throw logic_error(
-            "unhandled operation (opcode + operands types) " + to_string(static_cast<int>(node->get_opcode())) + " " +
-            to_string(static_cast<int>(arg1->get_term_type())) + " " +
-            to_string(static_cast<int>(arg2->get_term_type())));
+          throw logic_error("unhandled operation (opcode + operands types)");
 
         result_noise = max(arg1_noise, arg2_noise) + noise_growth_it->second;
       }
@@ -204,6 +205,7 @@ int ParameterSelector::simulate_noise_bfv(
         {
           if (node->get_opcode() != ir::OpCode::encrypt)
             throw logic_error("unary operation ciphertext node (not encrypt) with non-ciphertext operand");
+
           arg1_noise = 0;
         }
         else
@@ -231,8 +233,178 @@ int ParameterSelector::simulate_noise_bfv(
 }
 
 void ParameterSelector::insert_mod_switch_bfv(
-  vector<int> data_level_primes_sizes, unordered_map<string, int> &nodes_noise)
-{}
+  const vector<int> &data_level_primes_sizes, unordered_map<string, int> &nodes_noise, int safety_margin)
+{
+  auto make_leveled_node_label = [](const string &node_label, int level) -> string {
+    return node_label + "_level" + to_string(level);
+  };
+
+  struct LevelData
+  {
+    int justified_level;
+    int lowest_level;
+  };
+
+  unordered_map<string, LevelData> nodes_level_data;
+  int L = data_level_primes_sizes.size();
+
+  const auto &nodes = program_->get_dataflow_sorted_nodes(false);
+  for (const auto &node : nodes)
+  {
+    if (node->get_term_type() != ir::TermType::ciphertext)
+      continue;
+
+    auto node_noise_it = nodes_noise.find(node->get_label());
+    if (node_noise_it == nodes_noise.end())
+      throw logic_error("node noise not handled in noise simulation");
+
+    if (nodes_level_data.find(node->get_label()) != nodes_level_data.end())
+      throw logic_error("repeated node in dataflow_sorted_nodes");
+
+    if (!node->is_operation_node())
+      nodes_level_data.insert({node->get_label(), {L, L}});
+    else
+    {
+      int operands_level = L;
+      if (node->get_operands().size() == 2)
+      {
+        const auto &arg1 = node->get_operands()[0];
+        auto arg1_level_data_it = nodes_level_data.find(arg1->get_label());
+
+        const auto &arg2 = node->get_operands()[1];
+        auto arg2_level_data_it = nodes_level_data.find(arg2->get_label());
+
+        if (arg1->get_term_type() == ir::TermType::ciphertext && arg2->get_term_type() == ir::TermType::ciphertext)
+        {
+          if (arg1_level_data_it == nodes_level_data.end())
+            throw logic_error("parent handled before child");
+          LevelData &arg1_level_data = arg1_level_data_it->second;
+
+          if (arg2_level_data_it == nodes_level_data.end())
+            throw logic_error("parent handled before child");
+          LevelData &arg2_level_data = arg2_level_data_it->second;
+
+          if (arg1_level_data.justified_level > arg2_level_data.justified_level)
+          {
+            ir::Program::Ptr arg1_lowest_level =
+              program_->find_node_in_dataflow(make_leveled_node_label(arg1->get_label(), arg1_level_data.lowest_level));
+            if (arg1_lowest_level == nullptr)
+              throw logic_error("node lowest level not found in the data flow");
+
+            node->delete_operand_term(make_leveled_node_label(arg1->get_label(), arg1_level_data.justified_level));
+            while (arg1_level_data.lowest_level > arg2_level_data.justified_level)
+            {
+              --arg1_level_data.lowest_level;
+              ir::Program::Ptr mod_switch_node = program_->insert_operation_node_in_dataflow(
+                ir::OpCode::modswitch, vector<ir::Program::Ptr>({arg1_lowest_level}),
+                make_leveled_node_label(arg1->get_label(), arg1_level_data.lowest_level), ir::TermType::ciphertext);
+              arg1_lowest_level = mod_switch_node;
+            }
+            ir::Program::Ptr arg1_matching_level = program_->find_node_in_dataflow(
+              make_leveled_node_label(arg1->get_label(), arg2_level_data.justified_level));
+            if (arg1_matching_level == nullptr)
+              throw logic_error("leveled version of node supposed to be present");
+
+            node->add_operand(arg1_matching_level);
+          }
+          else if (arg1_level_data.justified_level < arg2_level_data.justified_level)
+          {
+            ir::Program::Ptr arg2_lowest_level =
+              program_->find_node_in_dataflow(make_leveled_node_label(arg2->get_label(), arg2_level_data.lowest_level));
+            if (arg2_lowest_level == nullptr)
+              throw logic_error("node lowest level not found in the data flow");
+
+            node->delete_operand_term(make_leveled_node_label(arg2->get_label(), arg2_level_data.justified_level));
+            while (arg2_level_data.lowest_level > arg1_level_data.justified_level)
+            {
+              --arg2_level_data.lowest_level;
+              ir::Program::Ptr mod_switch_node = program_->insert_operation_node_in_dataflow(
+                ir::OpCode::modswitch, vector<ir::Program::Ptr>({arg2_lowest_level}),
+                make_leveled_node_label(arg2->get_label(), arg2_level_data.lowest_level), ir::TermType::ciphertext);
+              arg2_lowest_level = mod_switch_node;
+            }
+            ir::Program::Ptr arg2_matching_level = program_->find_node_in_dataflow(
+              make_leveled_node_label(arg2->get_label(), arg2_level_data.justified_level));
+            if (arg2_matching_level == nullptr)
+              throw logic_error("leveled version of node supposed to be present");
+
+            node->add_operand(arg2_matching_level);
+          }
+        }
+
+        if (arg1->get_term_type() == ir::TermType::ciphertext)
+        {
+          if (arg1_level_data_it == nodes_level_data.end())
+            throw logic_error("parent handled before child");
+
+          operands_level = arg1_level_data_it->second.justified_level;
+        }
+        if (arg2->get_term_type() == ir::TermType::ciphertext)
+        {
+          if (arg2_level_data_it == nodes_level_data.end())
+            throw logic_error("parent handled before child");
+
+          operands_level = min(operands_level, arg2_level_data_it->second.justified_level);
+        }
+      }
+      else if (node->get_operands().size() == 1)
+      {
+        const auto &arg1 = node->get_operands()[0];
+        if (arg1->get_term_type() != ir::TermType::ciphertext)
+        {
+          if (node->get_opcode() != ir::OpCode::encrypt)
+            throw logic_error("unary operation ciphertext node (not encrypt) with non-ciphertext operand");
+
+          operands_level = L;
+        }
+        else
+        {
+          auto arg1_level_data_it = nodes_level_data.find(arg1->get_label());
+          if (arg1_level_data_it == nodes_level_data.end())
+            throw logic_error("parent handled before child");
+
+          operands_level = arg1_level_data_it->second.justified_level;
+        }
+      }
+
+      int node_level = operands_level;
+      if (node_level < 1)
+        throw logic_error("invalid node_level");
+
+      int node_adapted_noise = node_noise_it->second;
+      for (int i = L; i > node_level; --i)
+        node_adapted_noise -= data_level_primes_sizes[i - 1];
+
+      ir::Program::Ptr mod_switch_arg = node;
+      while (node_level > 0 && node_adapted_noise >= data_level_primes_sizes[node_level - 1] + safety_margin)
+      {
+        node_adapted_noise -= data_level_primes_sizes[node_level - 1];
+        --node_level;
+        ir::Program::Ptr mod_switch_node = program_->insert_operation_node_in_dataflow(
+          ir::OpCode::modswitch, vector<ir::Program::Ptr>({mod_switch_arg}),
+          make_leveled_node_label(node->get_label(), node_level), ir::TermType::ciphertext);
+        mod_switch_arg = mod_switch_node;
+      }
+
+      if (node_level < 1)
+        throw logic_error("invalid node_level");
+
+      if (node_level < operands_level)
+      {
+        for (const auto &parent_label : node->get_parents_labels())
+        {
+          ir::Program::Ptr parent = program_->find_node_in_dataflow(parent_label);
+          if (parent == nullptr)
+            throw logic_error("parent node not found");
+
+          parent->delete_operand_term(node->get_label());
+          parent->add_operand(mod_switch_arg);
+        }
+      }
+      nodes_level_data.insert({node->get_label(), {node_level, node_level}});
+    }
+  }
+}
 
 void apply_mod_switch_schedule(
   ir::Program *program, const unordered_map<string, tuple<size_t, size_t>> &nodes_noise,
@@ -260,6 +432,9 @@ void apply_mod_switch_schedule(
     for (const auto &parent_label : node->get_parents_labels())
     {
       ir::Program::Ptr parent = program->find_node_in_dataflow(parent_label);
+      if (parent == nullptr)
+        throw logic_error("parent node not found");
+
       parent->delete_operand_term(node_noise.first);
       parent->add_operand(mod_switch_arg);
     }
@@ -315,6 +490,73 @@ void apply_mod_switch_schedule(
       }
     }
   }
+}
+
+unordered_map<string, unordered_set<string>> ParameterSelector::get_outputs_composing_nodes() const
+{
+  unordered_map<string, unordered_set<string>> outputs_composing_nodes;
+
+  const auto &nodes = program_->get_dataflow_sorted_nodes(false);
+  for (const auto &node : nodes)
+  {
+    if (node->get_term_type() != ir::TermType::ciphertext)
+      continue;
+
+    if (node->is_operation_node())
+    {
+      auto [node_composing_nodes_it, node_composing_nodes_exists] =
+        outputs_composing_nodes.insert({node->get_label(), {}});
+
+      if (node_composing_nodes_exists)
+        throw logic_error("repeated node in dataflow_sorted_nodes");
+
+      if (node->get_operands().size() == 2)
+      {
+        const auto &arg1 = node->get_operands()[0];
+        const auto &arg2 = node->get_operands()[1];
+
+        if (arg1->get_term_type() == ir::TermType::ciphertext)
+        {
+          auto arg1_composing_nodes_it = outputs_composing_nodes.find(arg1->get_label());
+          if (arg1_composing_nodes_it != outputs_composing_nodes.end())
+          {
+            node_composing_nodes_it->second.insert(
+              arg1_composing_nodes_it->second.begin(), arg1_composing_nodes_it->second.end());
+            outputs_composing_nodes.erase(arg1_composing_nodes_it);
+          }
+          node_composing_nodes_it->second.insert(arg1->get_label());
+        }
+        if (arg2->get_term_type() == ir::TermType::ciphertext)
+        {
+          auto arg2_composing_nodes_it = outputs_composing_nodes.find(arg2->get_label());
+          if (arg2_composing_nodes_it != outputs_composing_nodes.end())
+          {
+            node_composing_nodes_it->second.insert(
+              arg2_composing_nodes_it->second.begin(), arg2_composing_nodes_it->second.end());
+            outputs_composing_nodes.erase(arg2_composing_nodes_it);
+          }
+          node_composing_nodes_it->second.insert(arg2->get_label());
+        }
+      }
+      else if (node->get_operands().size() == 1)
+      {
+        const auto &arg1 = node->get_operands()[0];
+        int arg1_noise;
+        if (arg1->get_term_type() == ir::TermType::ciphertext)
+        {
+          auto arg1_composing_nodes_it = outputs_composing_nodes.find(arg1->get_label());
+          if (arg1_composing_nodes_it != outputs_composing_nodes.end())
+          {
+            node_composing_nodes_it->second.insert(
+              arg1_composing_nodes_it->second.begin(), arg1_composing_nodes_it->second.end());
+            outputs_composing_nodes.erase(arg1_composing_nodes_it);
+          }
+          node_composing_nodes_it->second.insert(arg1->get_label());
+        }
+      }
+    }
+  }
+  return outputs_composing_nodes;
 }
 
 } // namespace param_selector
