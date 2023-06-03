74188bd4e74cf19e3eccc13982850ff966d9c9c5
nassimb0u <in_boulechfar@esi.dz>
2023-06-03 22:15:58 +0100

    porcupine dot product
    baseline not found but it's the same as the synthesized
    https://bitbucket.org/vip-benchmarks/vip-bench/src/he-branch/dot-product-bfv/dot-product.cpp

--- benchmarks/dot_product_porcupine/dot_product.cpp
+++ benchmarks/dot_product_porcupine/dot_product.cpp
@@ -1,30 +1,14 @@
-#include "fhecompiler.hpp"
-#include <cstddef>
+#include "fheco/fheco.hpp"
 #include <cstdint>
 #include <fstream>
 #include <iostream>
 #include <stdexcept>
 #include <string>
-#include <vector>
 
 using namespace std;
-using namespace fhecompiler;
+using namespace fheco;
 
 void dot_product_opt()
-{
-  Ciphertext c0("c0", -100, 100);
-  Ciphertext c00("c00", -100, 100);
-  Ciphertext c1 = c0 * c00;
-  Ciphertext c1_rot_4 = c1 << 4;
-  Ciphertext c2 = c1 + c1_rot_4;
-  Ciphertext c2_rot_1 = c2 << 1;
-  Ciphertext c3 = c2_rot_1 + c2;
-  Ciphertext c3_rot_2 = c3 << 2;
-  Ciphertext c_result = c3_rot_2 + c3;
-  c_result.set_output("c_result");
-}
-
-void dot_product_plain_opt()
 {
   Ciphertext c0("c0", -100, 100);
   Plaintext v1("v1", -100, 100);
@@ -38,51 +22,84 @@ void dot_product_plain_opt()
   c_result.set_output("c_result");
 }
 
+void print_bool_arg(bool arg, const string &name, ostream &os)
+{
+  os << (arg ? name : "no_" + name);
+}
+
 int main(int argc, char **argv)
 {
-  size_t vector_size = 1024;
+  bool call_quantifier = false;
   if (argc > 1)
-    vector_size = stoull(argv[1]);
+    call_quantifier = stoi(argv[1]);
 
-  bool is_cipher_cipher = true;
+  auto ruleset = Compiler::Ruleset::joined;
   if (argc > 2)
-  {
-    std::stringstream ss(argv[2]);
-    if (!(ss >> boolalpha >> is_cipher_cipher))
-      throw invalid_argument("could not parse is_cipher_cipher to bools");
-  }
+    ruleset = static_cast<Compiler::Ruleset>(stoi(argv[2]));
 
-  int trs_passes = 1;
+  auto rewrite_heuristic = trs::RewriteHeuristic::bottom_up;
   if (argc > 3)
-    trs_passes = stoi(argv[3]);
+    rewrite_heuristic = static_cast<trs::RewriteHeuristic>(stoi(argv[3]));
 
-  bool optimize = trs_passes > 0;
+  int64_t max_iter = 400000;
+  if (argc > 4)
+    max_iter = stoull(argv[4]);
 
-  cout << "vector_size: " << vector_size << ", "
-       << "trs_passes: " << trs_passes << '\n';
+  bool rewrite_created_sub_terms = true;
+  if (argc > 5)
+    rewrite_created_sub_terms = stoi(argv[5]);
+
+  bool cse = true;
+  if (argc > 6)
+    cse = stoi(argv[6]);
+
+  bool cse_order_operands = true;
+  if (argc > 7)
+    cse_order_operands = stoi(argv[7]);
+
+  bool const_folding = true;
+  if (argc > 8)
+    const_folding = stoi(argv[8]);
+
+  print_bool_arg(call_quantifier, "call_quantifier", clog);
+  clog << " ";
+  clog << ruleset << " " << rewrite_heuristic << " " << max_iter << " ";
+  print_bool_arg(rewrite_created_sub_terms, "rewrite_created_sub_terms", clog);
+  clog << " ";
+  print_bool_arg(cse, "cse", clog);
+  clog << " ";
+  print_bool_arg(cse_order_operands, "cse_order_operands", clog);
+  clog << " ";
+  print_bool_arg(const_folding, "const_folding", clog);
+  clog << '\n';
 
   string func_name = "dot_product";
-  Compiler::create_func(func_name, vector_size, 16, true, Scheme::bfv);
-  if (is_cipher_cipher)
-    dot_product_opt();
-  else
-    dot_product_plain_opt();
+  Compiler::create_func(func_name, 8, 16, false, false, false);
+
+  dot_product_opt();
+
   ofstream init_ir_os(func_name + "_init_ir.dot");
-  Compiler::draw_ir(init_ir_os);
-  const auto &rand_inputs = Compiler::get_example_input_values();
-  ofstream gen_code_os("he/gen_he_" + func_name + ".hpp");
-  if (optimize)
-    Compiler::compile(gen_code_os, trs_passes);
-  else
-    Compiler::compile_noopt(gen_code_os);
-  ofstream final_ir_os(func_name + "_final_ir.dot");
-  Compiler::draw_ir(final_ir_os);
-  gen_code_os.close();
-  auto outputs = Compiler::evaluate_on_clear(rand_inputs);
-  if (outputs != Compiler::get_example_output_values())
+  util::draw_ir(Compiler::active_func(), init_ir_os);
+
+  const auto &rand_inputs = Compiler::active_func()->data_flow().inputs_info();
+
+  Compiler::compile(ruleset, rewrite_heuristic, max_iter, rewrite_created_sub_terms);
+
+  auto outputs = util::evaluate_on_clear(Compiler::active_func(), rand_inputs);
+  if (outputs != Compiler::active_func()->data_flow().outputs_info())
     throw logic_error("compilation correctness-test failed");
 
   ofstream rand_example_os(func_name + "_rand_example.txt");
-  Compiler::print_inputs_outputs(rand_example_os);
+  util::print_io_terms_values(Compiler::active_func(), rand_example_os);
+
+  ofstream final_ir_os(func_name + "_final_ir.dot");
+  util::draw_ir(Compiler::active_func(), final_ir_os);
+
+  if (call_quantifier)
+  {
+    util::Quantifier quantifier1(Compiler::active_func());
+    quantifier1.run_all_analysis();
+    quantifier1.print_info(cout);
+  }
   return 0;
 }
