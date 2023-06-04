598c336232a79f494e86afc47cfe584ac8617e90
nassimb0u <in_boulechfar@esi.dz>
2023-06-04 19:44:47 +0100

    porcupine poly reg
    baseline not found but tried to reproduce it
    from the synthesized kernel and the counters provided in Porcupine paper
    https://bitbucket.org/vip-benchmarks/vip-bench/src/he-branch/poly-reg-bfv/poly-reg.cpp

--- benchmarks/poly_reg_porcupine/poly_reg.cpp
+++ benchmarks/poly_reg_porcupine/poly_reg.cpp
@@ -1,21 +1,31 @@
-#include "fhecompiler.hpp"
+#include "fheco/fheco.hpp"
 #include <cstdint>
 #include <fstream>
 #include <iostream>
 #include <stdexcept>
 #include <string>
-#include <vector>
 
 using namespace std;
-using namespace fhecompiler;
+using namespace fheco;
+
+void poly_reg_baseline()
+{
+  Ciphertext c0("c0", -100, 100);
+  Ciphertext c1("c1", -100, 100);
+  Ciphertext c2("c2", -100, 100);
+  Ciphertext c3("c3", -100, 100);
+  Ciphertext c4("c4", -100, 100);
+  Ciphertext c_result = c1 - (square(c0) * c4 + c0 * c3 + c2);
+  c_result.set_output("c_result");
+}
 
 void poly_reg_opt()
 {
-  Ciphertext c0("c0", -10, 10);
-  Ciphertext c1("c1", -10, 10);
-  Ciphertext c2("c2", -10, 10);
-  Ciphertext c3("c3", -10, 10);
-  Ciphertext c4("c4", -10, 10);
+  Ciphertext c0("c0", -100, 100);
+  Ciphertext c1("c1", -100, 100);
+  Ciphertext c2("c2", -100, 100);
+  Ciphertext c3("c3", -100, 100);
+  Ciphertext c4("c4", -100, 100);
   Ciphertext c5 = c0 * c4;
   Ciphertext c6 = c5 + c3;
   Ciphertext c7 = c0 * c6;
@@ -24,39 +34,99 @@ void poly_reg_opt()
   c_result.set_output("c_result");
 }
 
+void print_bool_arg(bool arg, const string &name, ostream &os)
+{
+  os << (arg ? name : "no_" + name);
+}
+
 int main(int argc, char **argv)
 {
-  size_t vector_size = 1024;
+  bool call_quantifier = false;
   if (argc > 1)
-    vector_size = stoi(argv[1]);
+    call_quantifier = stoi(argv[1]);
 
-  int trs_passes = 1;
+  auto ruleset = Compiler::Ruleset::joined;
   if (argc > 2)
-    trs_passes = stoi(argv[2]);
+    ruleset = static_cast<Compiler::Ruleset>(stoi(argv[2]));
 
-  bool optimize = trs_passes > 0;
+  auto rewrite_heuristic = trs::RewriteHeuristic::bottom_up;
+  if (argc > 3)
+    rewrite_heuristic = static_cast<trs::RewriteHeuristic>(stoi(argv[3]));
 
-  cout << "vector_size: " << vector_size << ", "
-       << "trs_passes: " << trs_passes << '\n';
+  int64_t max_iter = 400000;
+  if (argc > 4)
+    max_iter = stoull(argv[4]);
+
+  bool rewrite_created_sub_terms = true;
+  if (argc > 5)
+    rewrite_created_sub_terms = stoi(argv[5]);
+
+  bool cse = true;
+  if (argc > 6)
+    cse = stoi(argv[6]);
+
+  bool cse_order_operands = true;
+  if (argc > 7)
+    cse_order_operands = stoi(argv[7]);
+
+  bool const_folding = true;
+  if (argc > 8)
+    const_folding = stoi(argv[8]);
+
+  print_bool_arg(call_quantifier, "call_quantifier", clog);
+  clog << " ";
+  clog << ruleset << " " << rewrite_heuristic << " " << max_iter << " ";
+  print_bool_arg(rewrite_created_sub_terms, "rewrite_created_sub_terms", clog);
+  clog << " ";
+  print_bool_arg(cse, "cse", clog);
+  clog << " ";
+  print_bool_arg(cse_order_operands, "cse_order_operands", clog);
+  clog << " ";
+  print_bool_arg(const_folding, "const_folding", clog);
+  clog << '\n';
+
+  if (cse)
+    Compiler::enable_cse();
+  else
+    Compiler::disable_cse();
+
+  if (cse_order_operands)
+    Compiler::enable_order_operands();
+  else
+    Compiler::disable_order_operands();
+
+  if (const_folding)
+    Compiler::enable_const_folding();
+  else
+    Compiler::disable_const_folding();
 
   string func_name = "poly_reg";
-  Compiler::create_func(func_name, vector_size, 16, true, Scheme::bfv);
-  poly_reg_opt();
+  Compiler::create_func(func_name, 1024, 16, true, false, false);
+
+  poly_reg_baseline();
+
   ofstream init_ir_os(func_name + "_init_ir.dot");
-  Compiler::draw_ir(init_ir_os);
-  const auto &rand_inputs = Compiler::get_example_input_values();
-  ofstream gen_code_os("he/gen_he_" + func_name + ".hpp");
-  if (optimize)
-    Compiler::compile(gen_code_os, trs_passes);
-  else
-    Compiler::compile_noopt(gen_code_os);
-  ofstream final_ir_os(func_name + "_final_ir.dot");
-  Compiler::draw_ir(final_ir_os);
-  auto outputs = Compiler::evaluate_on_clear(rand_inputs);
-  if (outputs != Compiler::get_example_output_values())
+  util::draw_ir(Compiler::active_func(), init_ir_os);
+
+  const auto &rand_inputs = Compiler::active_func()->data_flow().inputs_info();
+
+  Compiler::compile(ruleset, rewrite_heuristic, max_iter, rewrite_created_sub_terms);
+
+  auto outputs = util::evaluate_on_clear(Compiler::active_func(), rand_inputs);
+  if (outputs != Compiler::active_func()->data_flow().outputs_info())
     throw logic_error("compilation correctness-test failed");
 
   ofstream rand_example_os(func_name + "_rand_example.txt");
-  Compiler::print_inputs_outputs(rand_example_os);
+  util::print_io_terms_values(Compiler::active_func(), rand_example_os);
+
+  ofstream final_ir_os(func_name + "_final_ir.dot");
+  util::draw_ir(Compiler::active_func(), final_ir_os);
+
+  if (call_quantifier)
+  {
+    util::Quantifier quantifier1(Compiler::active_func());
+    quantifier1.run_all_analysis();
+    quantifier1.print_info(cout);
+  }
   return 0;
 }
