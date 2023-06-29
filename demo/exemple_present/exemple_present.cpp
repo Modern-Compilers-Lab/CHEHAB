754c782f956927042eb9122c2433a1b2674e5b93
nassimb0u <in_boulechfar@esi.dz>
2023-06-29 21:22:52 +0100

    2. codegen (ir+code+correctness)

--- demo/exemple_present/exemple_present.cpp
+++ demo/exemple_present/exemple_present.cpp
@@ -16,13 +16,22 @@ int main(int argc, char **argv)
 
   const auto &fonc = Compiler::create_func(nom_app, nb_pos, l_bits, arith_signe, rotation_cycl);
 
-  Ciphertext a("a");
-  Ciphertext b("b");
-  Ciphertext c("c");
+  Ciphertext a("a", -10, 10);
+  Ciphertext b("b", -10, 10);
+  Ciphertext c("c", -10, 10);
   Ciphertext resultat = a * (b - c) + a * (0 + c);
   resultat.set_output("résultat");
 
-  ofstream flux_ri(nom_app + "_noopt_ir_nocodegen.dot");
+  string nom_gen = "gen_he_" + nom_app;
+  string chemin_gen = "he/" + nom_gen;
+  ofstream flux_entete(chemin_gen + ".hpp");
+  ofstream flux_source(chemin_gen + ".cpp");
+  Compiler::gen_he_code(fonc, flux_entete, nom_gen + ".hpp", flux_source);
+
+  ofstream flux_exemple_es(nom_app + "_io_example.txt");
+  util::print_io_terms_values(fonc, flux_exemple_es);
+
+  ofstream flux_ri(nom_app + "_noopt_ir.dot");
   util::draw_ir(fonc, flux_ri, true);
 
   return 0;
