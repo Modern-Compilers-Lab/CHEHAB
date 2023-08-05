1dfccaa5a091b19d86b84db0bbf340f30bbfd168
nassimb0u <in_boulechfar@esi.dz>
2023-08-05 23:24:28 +0100

    refactor linear_reg

---
+++ benchmarks/linear_reg/he/main.cpp
@@ -0,0 +1,84 @@
+#include <chrono>
+#include <cstddef>
+#include <fstream>
+#include <iostream>
+#include <ostream>
+#include "gen_he_linear_reg_noopt.hpp"
+#include "gen_he_linear_reg_opt.hpp"
+#include "utils.hpp"
+
+using namespace std;
+using namespace seal;
+
+void print_bool_arg(bool arg, const string &name, ostream &os)
+{
+  os << (arg ? name : "no" + name);
+}
+
+int main(int argc, char **argv)
+{
+  bool opt = true;
+  if (argc > 1)
+    opt = stoi(argv[1]);
+
+  print_bool_arg(opt, "opt", clog);
+  clog << '\n';
+
+  string app_name = "linear_reg";
+  ifstream is("../" + app_name + "_io_example.txt");
+  if (!is)
+    throw invalid_argument("failed to open io example file");
+
+  EncryptionParameters params(scheme_type::bfv);
+  size_t n = 8192;
+  params.set_poly_modulus_degree(n);
+  params.set_plain_modulus(PlainModulus::Batching(n, 20));
+  ClearArgsInfo clear_inputs, clear_outputs;
+  size_t func_slot_count;
+  parse_inputs_outputs_file(is, params.plain_modulus().value(), clear_inputs, clear_outputs, func_slot_count);
+  params.set_coeff_modulus(CoeffModulus::Create(n, {60, 60, 60}));
+  SEALContext context(params, true, sec_level_type::tc128);
+  BatchEncoder batch_encoder(context);
+  KeyGenerator keygen(context);
+  const SecretKey &secret_key = keygen.secret_key();
+  PublicKey public_key;
+  keygen.create_public_key(public_key);
+  RelinKeys relin_keys;
+  keygen.create_relin_keys(relin_keys);
+  GaloisKeys galois_keys;
+  if (opt)
+    keygen.create_galois_keys(get_rotation_steps_linear_reg_opt(), galois_keys);
+  else
+    keygen.create_galois_keys(get_rotation_steps_linear_reg_noopt(), galois_keys);
+  Encryptor encryptor(context, public_key);
+  Evaluator evaluator(context);
+  Decryptor decryptor(context, secret_key);
+
+  EncryptedArgs encrypted_inputs;
+  EncodedArgs encoded_inputs;
+  prepare_he_inputs(batch_encoder, encryptor, clear_inputs, encrypted_inputs, encoded_inputs);
+  EncryptedArgs encrypted_outputs;
+  EncodedArgs encoded_outputs;
+
+  chrono::high_resolution_clock::time_point t;
+  chrono::duration<double, milli> elapsed;
+  t = chrono::high_resolution_clock::now();
+  if (opt)
+    linear_reg_opt(
+      encrypted_inputs, encoded_inputs, encrypted_outputs, encoded_outputs, batch_encoder, encryptor, evaluator,
+      relin_keys, galois_keys);
+  else
+    linear_reg_noopt(
+      encrypted_inputs, encoded_inputs, encrypted_outputs, encoded_outputs, batch_encoder, encryptor, evaluator,
+      relin_keys, galois_keys);
+  elapsed = chrono::high_resolution_clock::now() - t;
+
+  ClearArgsInfo obtained_clear_outputs;
+  get_clear_outputs(
+    batch_encoder, decryptor, encrypted_outputs, encoded_outputs, func_slot_count, obtained_clear_outputs);
+  print_encrypted_outputs_info(context, decryptor, encrypted_outputs, clog);
+  if (clear_outputs != obtained_clear_outputs)
+    throw logic_error("clear_outputs != obtained_clear_outputs");
+
+  cout << elapsed.count() << " ms\n";
+}
---
+++ benchmarks/linear_reg/linear_reg_checks.cpp
@@ -0,0 +1,168 @@
+#include "fheco/fheco.hpp"
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <ostream>
+#include <stdexcept>
+#include <string>
+
+using namespace std;
+using namespace fheco;
+
+void linear_reg()
+{
+  Ciphertext c0("c0", -10, 10);
+  Ciphertext c1("c1", -10, 10);
+  Ciphertext c2("c2", -10, 10);
+  Ciphertext c3("c3", -10, 10);
+  Ciphertext c_result = c1 - (c2 * c0) - c3;
+  c_result.set_output("c_result");
+}
+
+void linear_reg_baseline()
+{
+  Ciphertext c0("c0", -10, 10);
+  Ciphertext c1("c1", -10, 10);
+  Ciphertext c2("c2", -10, 10);
+  Ciphertext c3("c3", -10, 10);
+  Ciphertext c_result = c1 - (c2 * c0) - c3;
+  c_result.set_output("c_result");
+}
+
+void print_bool_arg(bool arg, const string &name, ostream &os)
+{
+  os << (arg ? name : "no_" + name);
+}
+
+int main(int argc, char **argv)
+{
+  bool call_quantifier = false;
+  if (argc > 1)
+    call_quantifier = stoi(argv[1]);
+
+  auto ruleset = Compiler::Ruleset::joined;
+  if (argc > 2)
+    ruleset = static_cast<Compiler::Ruleset>(stoi(argv[2]));
+
+  auto rewrite_heuristic = trs::RewriteHeuristic::bottom_up;
+  if (argc > 3)
+    rewrite_heuristic = static_cast<trs::RewriteHeuristic>(stoi(argv[3]));
+
+  bool cse = true;
+  if (argc > 4)
+    cse = stoi(argv[4]);
+
+  bool const_folding = true;
+  if (argc > 5)
+    const_folding = stoi(argv[5]);
+
+  print_bool_arg(call_quantifier, "quantifier", clog);
+  clog << " ";
+  clog << "trs_" << ruleset << " " << rewrite_heuristic;
+  clog << " ";
+  print_bool_arg(cse, "cse", clog);
+  clog << " ";
+  print_bool_arg(const_folding, "const_folding", clog);
+  clog << '\n';
+
+  string app_name = "linear_reg";
+  size_t slot_count = 1024;
+  int bit_width = 20;
+  bool signdness = true;
+  bool need_cyclic_rotation = false;
+
+  clog << "\nnoopt function\n";
+  string noopt_func_name = app_name + "_noopt";
+  const auto &noopt_func =
+    Compiler::create_func(noopt_func_name, slot_count, bit_width, signdness, need_cyclic_rotation);
+  linear_reg();
+
+  string noopt_gen_name = "gen_he_" + noopt_func_name;
+  string noopt_gen_path = "he/" + noopt_gen_name;
+  ofstream noopt_header_os(noopt_gen_path + ".hpp");
+  if (!noopt_header_os)
+    throw logic_error("failed to create noopt_header file");
+
+  ofstream noopt_source_os(noopt_gen_path + ".cpp");
+  if (!noopt_source_os)
+    throw logic_error("failed to create noopt_source file");
+
+  Compiler::gen_he_code(noopt_func, noopt_header_os, noopt_gen_name + ".hpp", noopt_source_os);
+
+  ofstream noopt_ir_os(noopt_func_name + "_ir.dot");
+  if (!noopt_ir_os)
+    throw logic_error("failed to create noopt_ir file");
+
+  util::draw_ir(noopt_func, noopt_ir_os);
+  util::Quantifier noopt_quantifier(noopt_func);
+  if (call_quantifier)
+  {
+    cout << "\ninitial circuit characteristics\n";
+    noopt_quantifier.run_all_analysis();
+    noopt_quantifier.print_info(cout);
+    cout << endl;
+  }
+
+  clog << "\nopt function\n";
+  if (cse)
+  {
+    Compiler::enable_cse();
+    Compiler::enable_order_operands();
+  }
+  else
+  {
+    Compiler::disable_cse();
+    Compiler::disable_order_operands();
+  }
+
+  if (const_folding)
+    Compiler::enable_const_folding();
+  else
+    Compiler::disable_const_folding();
+
+  string opt_func_name = app_name + "_opt";
+  const auto &opt_func = Compiler::create_func(opt_func_name, slot_count, bit_width, signdness, need_cyclic_rotation);
+  linear_reg_baseline();
+
+  string opt_gen_name = "gen_he_" + opt_func_name;
+  string opt_gen_path = "he/" + opt_gen_name;
+  ofstream opt_header_os(opt_gen_path + ".hpp");
+  if (!opt_header_os)
+    throw logic_error("failed to create opt_header file");
+
+  ofstream opt_source_os(opt_gen_path + ".cpp");
+  if (!opt_source_os)
+    throw logic_error("failed to create opt_source file");
+
+  Compiler::compile(opt_func, ruleset, rewrite_heuristic, opt_header_os, opt_gen_name + ".hpp", opt_source_os);
+
+  auto noopt_obtained_outputs = util::evaluate_on_clear(noopt_func, opt_func->get_inputs_example_values());
+  auto opt_obtained_outputs = util::evaluate_on_clear(opt_func, noopt_func->get_inputs_example_values());
+  if (
+    noopt_obtained_outputs != opt_func->get_outputs_example_values() ||
+    opt_obtained_outputs != noopt_func->get_outputs_example_values())
+    throw logic_error("compilation correctness-test failed");
+
+  ofstream io_example_os(app_name + "_io_example.txt");
+  if (!io_example_os)
+    throw logic_error("failed to create io_example file");
+
+  util::print_io_terms_values(noopt_func, io_example_os);
+  ofstream opt_ir_os(opt_func_name + "_ir.dot");
+  if (!opt_ir_os)
+    throw logic_error("failed to create opt_ir file");
+
+  util::draw_ir(opt_func, opt_ir_os);
+  if (call_quantifier)
+  {
+    cout << "\nfinal circuit characteristics\n";
+    util::Quantifier opt_quantifier(opt_func);
+    opt_quantifier.run_all_analysis();
+    opt_quantifier.print_info(cout);
+
+    cout << "\nimprovement rates\n";
+    auto diff_quantifier = (noopt_quantifier - opt_quantifier) / noopt_quantifier * 100;
+    diff_quantifier.print_info(cout);
+  }
+  return 0;
+}
